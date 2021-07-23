560bcb5cfceabdc9d990866275da217e4978a410
qubridge <qubridge@DESKTOP-NF5FOT9>
2021-07-23 17:54:30 +0900

    cc

--- baekjoon/n009_10869/Main.java
+++ baekjoon/n009_10869/Main.java
@@ -7,7 +7,7 @@ public class Main{
 	//두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 	
 		
 	Scanner scan = new Scanner(System.in);
-	int a, b;
+	int a, b;  
 	a = scan.nextInt();
 	b = scan.nextInt();
 	
