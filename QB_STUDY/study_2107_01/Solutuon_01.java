0474be27b1ac8f1a5e135443d342472c6980383d
wooheejeon <77185171+wooheejeon@users.noreply.github.com>
2021-09-13 16:06:01 +0900

    Update Solutuon_01.java

--- QB_STUDY/study_2107_01/Solutuon_01.java
+++ QB_STUDY/study_2107_01/Solutuon_01.java
@@ -1,6 +1,11 @@
 package study_2107_01;
 
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Solutuon_01{
 	public static void main(String args[]){
 	
@@ -20,6 +25,25 @@ public class Solutuon_01{
 //		출력 값
 //		7 35
 		
+		
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+	Integer.parseInt(br.readLine());	//첫 줄 N 은 안쓰이므로 입력만 받는다.
+	StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+	int max = -1000001;
+	int min = 1000001;
+
+	while(st.hasMoreTokens()) {
+		int val = Integer.parseInt(st.nextToken());
+		if(val>max) {
+			max = val;
+		}
+		if(val<min) {
+			min = val;
+		}
+	}
+	System.out.println(min + " " + max);	
 		
 	}
 }
